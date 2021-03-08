784063aeed31a06761ae8855a73d73a4328028d7
SS-0415 <a32354990@gmail.com>
2021-03-08 21:15:45 +0800

    arrrr

--- app/src/main/java/com/example/myapplication/lz120page.java
+++ app/src/main/java/com/example/myapplication/lz120page.java
@@ -5,128 +5,100 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CompoundButton;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class lz120page extends AppCompatActivity implements View.OnClickListener {
     public int total = 0;
-    private boolean checked = false, vegansausage = false, bff1 = false, bff2 = false;
-    private TextView lz120amount;
-    private RadioGroup lz120snack1RB, lz120snack2RB, lz120sausage;
-    private RadioButton lz120vegan, lz120bff, lz120bff2, lz120beef, lz120pork, lz120sff, lz120sff2;
-    private int amount = 0, price = 54;
-
+    private int amount = 0, price = 72;
+    CheckBox porkbelly, cheesericecake, dumplings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lz120page);
 
-        lz120amount = findViewById(R.id.lz120amount);
-        ImageButton lz120minus = findViewById(R.id.lz120minus);
-        lz120minus.setOnClickListener(this);
-        ImageButton lz120plus = findViewById(R.id.lz120plus);
-        lz120plus.setOnClickListener(this);
-        ImageButton lz120shoppingcart = findViewById(R.id.lz120shoppingcart);
-        lz120shoppingcart.setOnClickListener(this);
-        lz120sausage = findViewById(R.id.lz120sausage);
-        lz120snack1RB = findViewById(R.id.lz120snack1RB);
-        lz120snack2RB = findViewById(R.id.lz120snack2RB);
 
-        lz120beef = findViewById(R.id.lz120beef);
-        lz120beef.setOnCheckedChangeListener(mOncheckedChangeListener);
-        lz120vegan = findViewById(R.id.lz120vegan);
-        lz120vegan.setOnCheckedChangeListener(mOncheckedChangeListener);
-        lz120pork = findViewById(R.id.lz120pork);
-        lz120pork.setOnCheckedChangeListener(mOncheckedChangeListener);
+        ImageButton whiteminus = findViewById(R.id.whiteminus);
+        whiteminus.setOnClickListener(this);
+        ImageButton whiteplus = findViewById(R.id.whiteplus);
+        whiteplus.setOnClickListener(this);
+        ImageButton whiteshoppingcart = findViewById(R.id.whiteshoppingcart);
+        whiteshoppingcart.setOnClickListener(this);
+        porkbelly = findViewById(R.id.porkBelly);
+        porkbelly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((CheckBox) v).isChecked();
+                switch (v.getId()) {
+                    case R.id.porkBelly:
+                        if (checked)
+                            price += 25;
+                        else
+                            price -= 25;
+                        break;
+                }
+            }
+        });
+        cheesericecake = findViewById(R.id.cheeseRicecake);
+        cheesericecake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((CheckBox) v).isChecked();
+                switch (v.getId()){
+                    case R.id.cheeseRicecake:
+                        if (checked)
+                            price += 12;
+                        else
+                            price -= 12;
+                        break;
+                }
+            }
+        });
+        dumplings = findViewById(R.id.dumplings);
+        dumplings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((CheckBox) v).isChecked();
+                switch (v.getId()){
+                    case R.id.dumplings:
+                        if (checked)
+                            price += 30;
+                        else
+                            price -= 30;
+                        break;
+                }
+            }
+        });
 
-
-        lz120bff = findViewById(R.id.lz120bff);
-        lz120bff.setOnCheckedChangeListener(mOncheckedChangeListener);
-        lz120sff = findViewById(R.id.lz120sff);
-        lz120sff.setOnCheckedChangeListener(mOncheckedChangeListener);
-
-
-        lz120bff2 = findViewById(R.id.lz120bff2);
-        lz120bff2.setOnCheckedChangeListener(mOncheckedChangeListener);
-        lz120sff2 = findViewById(R.id.lz120sff2);
-        lz120sff2.setOnCheckedChangeListener(mOncheckedChangeListener);
     }
+    public void onClick (View v){
 
-
-    public void onClick(View v) {
+        TextView white_amount = findViewById(R.id.white_amount);
         Intent it = new Intent();
-        switch (v.getId()) {
-            case R.id.lz120plus:
-                if (amount < 100) {
-                    amount++;
-                    lz120amount.setText(amount + "");
-                }
-                break;
-            case R.id.lz120minus:
-                if (amount > 0) {
+        switch (v.getId()){
+            case R.id.whiteminus:
+                if (amount > 0)
                     amount--;
-                    lz120amount.setText(amount + "");
-                }
+                else
+                    amount = 0;
+                white_amount.setText(Integer.toString(amount));
                 break;
-            case R.id.lz120shoppingcart:
-                if (amount > 0) {
-                    if (vegansausage)
-                        price+=18;
-                    total += price * amount;
-                    Toast.makeText(getApplicationContext(), "Total price: " + total, Toast.LENGTH_SHORT).show();
-                }
-                it.setClass(lz120page.this, MainActivity.class);
+            case R.id.whiteplus:
+                if (amount < 99)
+                    amount++;
+                else
+                    amount = 99;
+                white_amount.setText(Integer.toString(amount));
+                break;
+            case R.id.whiteshoppingcart:
+                if(amount!=0)
+                    Toast.makeText(lz120page.this,"Total amount is: "+Integer.toString(amount*price),Toast.LENGTH_LONG).show();
+                it.setClass(lz120page.this,MainActivity.class);
                 startActivity(it);
                 finish();
                 break;
         }
     }
-
-    private CompoundButton.OnCheckedChangeListener mOncheckedChangeListener = new CompoundButton.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(CompoundButton group, boolean checkedId) {
-            if (checked) return;
-            if (group.getId() == R.id.lz120sausage) {
-                checked = true;
-                lz120snack1RB.clearCheck();
-                lz120snack2RB.clearCheck();
-                checked = false;
-            } else if (group.getId() == R.id.lz120snack1RB) {
-                checked = true;
-                lz120snack2RB.clearCheck();
-                lz120sausage.clearCheck();
-                checked = false;
-            } else if (group.getId() == R.id.lz120snack2RB) {
-                checked = true;
-                lz120sausage.clearCheck();
-                lz120snack1RB.clearCheck();
-                checked = false;
-            }
-            switch (group.getId()){
-                case R.id.lz120vegan:
-                    vegansausage = true;
-                    break;
-                case R.id.lz120beef:
-                case R.id.lz120pork:
-                    vegansausage = false;
-                    break;
-                case R.id.lz120bff:
-                    bff1=true;
-                    break;
-                case R.id.lz120sff:
-                    bff1=false;
-                    break;
-                case R.id.lz120bff2:
-                    bff2=true;
-                    break;
-                case R.id.lz120sff2:
-                    bff2 = false;
-                    break;
-            }
-        }
-    };
 }
