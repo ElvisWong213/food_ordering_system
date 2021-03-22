8f93b26681f96844719ca236d834f68d8b66f0bb
SS-0415 <a32354990@gmail.com>
2021-03-22 15:26:29 +0800

    drinkpage done

--- app/src/main/java/com/example/myapplication/drinkpage.java
+++ app/src/main/java/com/example/myapplication/drinkpage.java
@@ -1,25 +1,99 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class drinkpage extends AppCompatActivity implements View.OnClickListener {
-
+    order store;
+    RadioButton Rramune, Rgreentea;
+    private int amount = 0, price = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinkpage);
+        ImageButton drinkminus = findViewById(R.id.drinkminus);
+        drinkminus.setOnClickListener(this);
+        ImageButton drinkplus = findViewById(R.id.drinkplus);
+        drinkplus.setOnClickListener(this);
+        ImageButton drinkshoppingcart = findViewById(R.id.drinkshoppingcart);
+        drinkshoppingcart.setOnClickListener(this);
         ImageButton drinkhomepage = findViewById(R.id.drinkhomepage);
         drinkhomepage.setOnClickListener(this);
+        Rramune = findViewById(R.id.Rramune);
+        Rgreentea = findViewById(R.id.Rgreentea);
+        store = new order(0,"",0,false,false,false,false,false, false, false);
+        Rramune.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((RadioButton) v).isChecked();
+                if (checked){
+                    store.setRamune(true);
+                    store.setGreentea(false);
+                    price = 23;
+                }
+            }
+        });
+        Rgreentea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean checked = ((RadioButton) v).isChecked();
+                if (checked){
+                    store.setRamune(false);
+                    store.setGreentea(true);
+                    price = 21;
+                }
+            }
+        });
     }
 
     public void onClick (View v){
+        int index = globalvariable.numOfOrder;
+        int k = globalvariable.numOfac;
+        TextView drink_amount = findViewById(R.id.drink_amount);
         Intent it = new Intent();
         switch (v.getId()){
+            case R.id.drinkminus:
+                if (amount > 0) {
+                    amount--;
+                    store.setOrder_amount(amount);
+                }
+                else{
+                    amount = 0;
+                    store.setOrder_amount(amount);
+                }
+                drink_amount.setText(Integer.toString(amount));
+                break;
+            case R.id.drinkplus:
+                if (amount < 99) {
+                    amount++;
+                    store.setOrder_amount(amount);
+                }
+                else{
+                    amount = 99;
+                    store.setOrder_amount(amount);
+                }
+                drink_amount.setText(Integer.toString(amount));
+                break;
+            case R.id.drinkshoppingcart:
+                if(amount!=0){
+                    if (globalvariable.firstOrder) {
+                        if (!globalvariable.login)
+                            globalvariable.ac[k] = new account("","","","","");
+                        globalvariable.firstOrder = false;
+                        globalvariable.ac[k].setStartnum(index);
+                    }
+                    store.setPrice(amount*price);
+                    globalvariable.ordering[index] = new order(amount,"null",store.getPrice(),store.getporkbelly(),store.getcheesericecake(),store.getDumpling(),store.getramune(),store.getgreentea(),store.getEgg(),store.getCharsiu());
+                    globalvariable.numOfOrder++;
+                    Toast.makeText(drinkpage.this,"Total amount is: "+Integer.toString(amount*price),Toast.LENGTH_LONG).show();
+                }
             case R.id.drinkhomepage:
                 it.setClass(drinkpage.this,MainActivity.class);
                 startActivity(it);
